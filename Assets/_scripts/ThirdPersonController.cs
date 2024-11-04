eee8a984a8f3d8671c966a9e3747cfd855661c9e
Paul Standing <paultobiasstanding@gmail.com>
2024-11-04 15:03:31 +0200

    ThirdPersonController Adds y input threshold to consider player should walk backwards and some other script tidying. Also added some basic textures.
    - Made all the ThirdPersonController inspector variables private with SerializeField
    - Added variable to adjust y input threshold for when player should walk backwards.
    - Added some basic texture to use to make testing easier, since you can visualise the movement better

--- Assets/_scripts/ThirdPersonController.cs
+++ Assets/_scripts/ThirdPersonController.cs
@@ -16,48 +16,50 @@ namespace StarterAssets
 	{
 		[Header("Player")]
 		[Tooltip("Move speed of the character in m/s")]
-		public float MoveSpeed = 2.0f;
+		[SerializeField] private float MoveSpeed = 2.0f;
 
 		[Tooltip("Sprint speed of the character in m/s")]
-		public float SprintSpeed = 5.335f;
+		[SerializeField] private float SprintSpeed = 5.335f;
 
 		[Tooltip("How fast the character turns to face movement direction")]
 		[Range(0.0f, 0.3f)]
-		public float RotationSmoothTime = 0.12f;
+		[SerializeField] private float RotationSmoothTime = 0.12f;
 
 		[Tooltip("Acceleration and deceleration")]
-		public float SpeedChangeRate = 10.0f;
+		[SerializeField] private float SpeedChangeRate = 10.0f;
 
-		public AudioClip LandingAudioClip;
-		public AudioClip[] FootstepAudioClips;
-		[Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+		[SerializeField] private AudioClip LandingAudioClip;
+		[SerializeField] private AudioClip[] FootstepAudioClips;
+		[Range(0, 1)][SerializeField] private float FootstepAudioVolume = 0.5f;
 
 		[Space(10)]
 		[Tooltip("The height the player can jump")]
-		public float JumpHeight = 1.2f;
+		[SerializeField] private float JumpHeight = 1.2f;
 
 		[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-		public float Gravity = -15.0f;
+		[SerializeField] private float Gravity = -15.0f;
 
 		[Space(10)]
 		[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-		public float JumpTimeout = 0.50f;
+		[SerializeField] private float JumpTimeout = 0.50f;
 
 		[Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-		public float FallTimeout = 0.15f;
+		[SerializeField] private float FallTimeout = 0.15f;
 
 		[Header("Player Grounded")]
 		[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-		public bool Grounded = true;
+		[SerializeField] private bool Grounded = true;
 
 		[Tooltip("Useful for rough ground")]
-		public float GroundedOffset = -0.14f;
+		[SerializeField] private float GroundedOffset = -0.14f;
 
 		[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-		public float GroundedRadius = 0.28f;
+		[SerializeField] private float GroundedRadius = 0.28f;
 
 		[Tooltip("What layers the character uses as ground")]
-		public LayerMask GroundLayers;
+		[SerializeField] private LayerMask GroundLayers;
+
+		[Range(0, -1.0f)][SerializeField] private float walkBackwardsInputYPosition = 0.0f;
 
 		// player
 		private float _speed;
@@ -207,7 +209,7 @@ namespace StarterAssets
 			inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
 			// If the requested direction is behind us, we want to walk backwards
-			if (_input.move.y < -0.5f)
+			if (_input.move.y < walkBackwardsInputYPosition)
 			{
 				_targetRotation = Mathf.Atan2(-inputDirection.x, -inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
 
